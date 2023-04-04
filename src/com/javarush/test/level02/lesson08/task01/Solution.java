13e1a62fd0c38bf7eafb79e94a887642500215c0
lykhvar.v <viktoriia.lykhvar@nixs.com>
2023-04-04 16:45:56 +0300

    ok

--- src/com/javarush/test/level02/lesson08/task01/Solution.java
+++ src/com/javarush/test/level02/lesson08/task01/Solution.java
@@ -1,22 +1,24 @@
 package com.javarush.test.level02.lesson08.task01;
 
 /**
- Написать функцию, которая вычисляет минимум из двух чисел.
+ * Написать функцию, которая вычисляет минимум из двух чисел.
  */
 public class Solution {
 
     public static void main(String[] args) {
         int a = 3;
         int b = 7;
-        System.out.print(min(a,b));
+        System.out.print(min(a, b));
     }
 
 
     public static int min(int a, int b) {
-        int min=0;
-        // напишите код здесь:
-
-
+        int min = 0;
+        if (a < b) {
+            min = a;
+        } else {
+            min = b;
+        }
         return min;
     }
 }
