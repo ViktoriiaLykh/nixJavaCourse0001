959b32bf83636e6b17ea734e0cb2bb37c899a5de
lykhvar.v <viktoriia.lykhvar@nixs.com>
2023-04-07 17:04:48 +0300

    Done

--- src/com/javarush/test/level04/lesson06/task01/Solution.java
+++ src/com/javarush/test/level04/lesson06/task01/Solution.java
@@ -4,8 +4,23 @@ package com.javarush.test.level04.lesson06.task01;
 Ввести с клавиатуры два числа, и вывести на экран минимальное из них.
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class Solution {
-  // напишите код здесь:
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+
+        if (num1 < num2) {
+            System.out.println(num1);
+        } else {
+            System.out.println(num2);
+        }
+    }
 
 
 }
--- src/com/javarush/test/level04/lesson06/task05/Solution.java
+++ src/com/javarush/test/level04/lesson06/task05/Solution.java
@@ -4,10 +4,26 @@ package com.javarush.test.level04.lesson06.task05;
 Ввести с клавиатуры имя и возраст. Если возраст меньше 18 вывести надпись «Подрасти еще».
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class Solution {
 
-  // напишите код здесь:
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+        if (age < 18) {
+            System.out.println(name + ", подрасти еще");
+        } else {
+            System.out.println("Можно");
+        }
+
+
+    }
 
 
 }
